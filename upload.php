4a8e0c4ea13ff867fbeaa96357fbb2dad0a24806
Ryan Owens <11846355+rwowens@users.noreply.github.com>
2020-06-20 22:39:11 -0400

    Adding contest column to DB; combining debug

--- upload.php
+++ upload.php
@@ -5,7 +5,13 @@ if (UPLOAD_PASSWORD != $_SERVER['PHP_AUTH_PW']) {
 	die('Invalid username or password.');
 }
 
-$xml = simplexml_load_file('php://input') or die('Unable to parse XML');
+if ($debug == true) {
+	$entityBody = file_get_contents('php://input');
+	var_dump($entityBody);
+	$xml = simplexml_load_string($entityBody) or die('Unable to parse XML');
+} else {
+	$xml = simplexml_load_file('php://input') or die('Unable to parse XML');
+}
 
 if ($xml !== false && strcasecmp($xml->call, $_SERVER['PHP_AUTH_USER']) == 0) {
 	$callsign = filter_var($xml->call, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -20,12 +26,14 @@ if ($xml !== false && strcasecmp($xml->call, $_SERVER['PHP_AUTH_USER']) == 0) {
 	$ops = filter_var($xml->class['ops'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 	$xmtrs = filter_var($xml->class['transmitter'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 	$power = filter_var($xml->class['power'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+	$contest = filter_var($xml->contest, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-	if (!($callsign && $club && $arrlSection)) {
+	if (!($callsign && $club && $arrlSection && $contest)) {
 		http_response_code(400);
 		if (!$callsign) echo('Callsign is missing; ');
 		if (!$club) echo('Club name is missing; ');
 		if (!$arrlSection) echo('ARRL Section is missing; ');
+		if (!$contest) echo('Contest value is missing; ');
 		die('Invalid input provided');
 	}
 	
@@ -35,7 +43,7 @@ if ($xml !== false && strcasecmp($xml->call, $_SERVER['PHP_AUTH_USER']) == 0) {
 
 		$db->beginTransaction();
 
-		$sql_scores = 'INSERT INTO scores (callsign, lst_upd, score, mult, qso, arrl_section, cq_zone, club, mode, bands, ops, xmtrs, power) VALUES (:callsign, :lstUpd, :score, :mult, :qso, :arrlSection, :cqZone, :club, :mode, :bands, :ops, :xmtrs, :power)';
+		$sql_scores = 'INSERT INTO scores (callsign, lst_upd, score, mult, qso, arrl_section, cq_zone, club, mode, bands, ops, xmtrs, power, contest) VALUES (:callsign, :lstUpd, :score, :mult, :qso, :arrlSection, :cqZone, :club, :mode, :bands, :ops, :xmtrs, :power, :contest)';
 		$stmt = $db->prepare($sql_scores);
 		$stmt->bindParam('callsign', $callsign);
 		$stmt->bindParam('lstUpd', $xml->timestamp);
@@ -50,11 +58,12 @@ if ($xml !== false && strcasecmp($xml->call, $_SERVER['PHP_AUTH_USER']) == 0) {
 		$stmt->bindParam('ops', $ops);
 		$stmt->bindParam('xmtrs', $xmtrs);
 		$stmt->bindParam('power', $power);
+		$stmt->bindParam('contest', $contest);
 		if ($stmt->execute() != TRUE) {
 			http_response_code(500);
 		}
 
-		$sql_scores = 'INSERT INTO scores_current (callsign, lst_upd, score, mult, qso, arrl_section, cq_zone, club, mode, bands, ops, xmtrs, power) VALUES (:callsign, :lstUpd, :score, :mult, :qso, :arrlSection, :cqZone, :club, :mode, :bands, :ops, :xmtrs, :power) ON DUPLICATE KEY UPDATE lst_upd=VALUES(lst_upd), score=VALUES(score), mult=VALUES(mult), qso=VALUES(qso), arrl_section=VALUES(arrl_section), cq_zone=VALUES(cq_zone), club=VALUES(club), mode=VALUES(mode), bands=VALUES(bands), ops=VALUES(ops), xmtrs=VALUES(xmtrs), power=VALUES(power)';
+		$sql_scores = 'INSERT INTO scores_current (callsign, lst_upd, score, mult, qso, arrl_section, cq_zone, club, mode, bands, ops, xmtrs, power, contest) VALUES (:callsign, :lstUpd, :score, :mult, :qso, :arrlSection, :cqZone, :club, :mode, :bands, :ops, :xmtrs, :power, :contest) ON DUPLICATE KEY UPDATE lst_upd=VALUES(lst_upd), score=VALUES(score), mult=VALUES(mult), qso=VALUES(qso), arrl_section=VALUES(arrl_section), cq_zone=VALUES(cq_zone), club=VALUES(club), mode=VALUES(mode), bands=VALUES(bands), ops=VALUES(ops), xmtrs=VALUES(xmtrs), power=VALUES(power), contest=VALUES(contest)';
 		$stmt = $db->prepare($sql_scores);
 		$stmt->bindParam('callsign', $callsign);
 		$stmt->bindParam('lstUpd', $xml->timestamp);
@@ -69,6 +78,7 @@ if ($xml !== false && strcasecmp($xml->call, $_SERVER['PHP_AUTH_USER']) == 0) {
 		$stmt->bindParam('ops', $ops);
 		$stmt->bindParam('xmtrs', $xmtrs);
 		$stmt->bindParam('power', $power);
+		$stmt->bindParam('contest', $contest);
 
 		if ($stmt->execute() != TRUE) {
 			http_response_code(500);
@@ -99,6 +109,7 @@ QSO Total: <?php echo $qso;?><br/>
 Multiplier: <?php echo $mult;?><br/>
 Score: <?php echo $score;?><br/>
 Last Update: <?php echo $xml->timestamp;?><br/>
+Contest: <?php echo $contest;?><br/>
 </body>
 </html>
 <?php
